a3d2d33b2e093bcc650fa607796d529d18dce670
Stefanus Hinardi <shinardi@microsoft.com>
2021-02-21 18:59:47 -0800

    via github

--- HttpTrigger1/__init__.py
+++ HttpTrigger1/__init__.py
@@ -16,9 +16,9 @@ def main(req: func.HttpRequest) -> func.HttpResponse:
             name = req_body.get('name')
 
     if name:
-        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully via github.")
     else:
         return func.HttpResponse(
-             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response via github.",
              status_code=200
         )
